0bf4be7d89d6194213a9b4ee13d0d68b0b33922f
Tajeshwar Khara <tkhara@gmail.com>
2016-09-01 15:36:48 -0700

    Pasted the whole JS code
    Let's see if we can see the JS file now...

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
 var map;
 
 function displayLocation(position) {
-  var latitude = position.coords.latitude;
-  var longitude = position.coords.longitude;
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
 
-  var pLocation = document.getElementById("location");
-  pLocation.innerHTML = latitude + ", " + longitude;
-  showMap(position.coords);
+	var pLocation = document.getElementById("location");
+	pLocation.innerHTML = latitude + ", " + longitude;
+
+	showMap(position.coords);
 }
 
 function showMap(coords) {
-  var googleLatLong = new google.maps.LatLng(coords.latitude, coords.longitude);
+	var googleLatLong = new google.maps.LatLng(coords.latitude, coords.longitude);
+	var mapOptions = {
+		zoom: 11,
+		center: googleLatLong,
+		mapTypeId: google.maps.MapTypeId.TERRAIN
+	};
 
-  var mapOptions = {
-    zoom: 11,
-    center: googleLatLong;
-    mapTypeId: google.maps.MapTypeId.TERRAIN
-  };
-
-  var mapDiv = document.getElementById("map");
-  var map = new google.maps.Map(mapDiv, mapOptions);
+	var mapDiv = document.getElementById("map");
+	map = new google.maps.Map(mapDiv, mapOptions);
 }
-
-
 
 function displayError(error) {
-  var errors = ["Unknown error", "Permission denied by user", "Position not available", "Timeout error"];
-  var message = errors[error.code];
-  console.warn("Error in getting your location: " + message, error.message);
+	var errors = ["Unknown error", "Permission denied by user", "Position not available", "Timeout error"];
+	var message = errors[error.code];
+	console.warn("Error in getting your location: " + message, error.message);
 }
 
-
 window.onload = function() {
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(displayLocation, displayError);
-  } else {
-    alert("Sorry, this browser doesn't support geolocation!");
-  }
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+	} else {
+		alert("Sorry, this browser doesn't support geolocation!");
+	}
 }
